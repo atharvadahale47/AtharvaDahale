3f444a16c8a51d3b70aceba5ddde94ac9fc97be6
atharvadahale47 <atharvadahale47@gmail.com>
2026-08-24 13:19:41 +0530

    Create lab.cpp

---
+++ lab.cpp
@@ -0,0 +1,46 @@
+include <iostream>
+using namespace std;
+int main()
+{
+            id1, id2, id3;
+          string title1, title2, title3;
+// BOOK 1 
+ cout<<"Enter Book 1 ID:";
+ cin >>idl;
+
+ cin.ignore();
+cout<<"Enter Book 1 Title:";
+getline(cin,title1);
+
+//Book 2
+cout<<"Enter Book 2 ID:";
+ cin >>idl;
+ cin.ignore();
+cout<<"Enter Book 2 Title:"; 
+getline(cin,title2); 
+
+//Book 3
+
+cout<<"Enter Book 3 ID:";
+ cin >>id3;
+
+ cin.ignore();
+cout<<"Enter Book 3 Title:" 
+getline(cin,title3);
+
+
+
+ 
+//Display books 
+cout<<"\nBook ID: "<<idl;
+cout<<"\nBook Title: "<< title1;
+
+cout<<"\nBook ID: "<<id2;
+cout<<"\nBook Title: :" <<title2;
+
+cout<<"\nBook ID: " <<id3;
+cout<<"\nBook Title: " <<title3;
+
+return 0;
+}
+}
